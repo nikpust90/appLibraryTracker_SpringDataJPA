d75be17da5420e1653210758ed7e45e06af2bb57
nikpust90 <nikolai.first21@yandex.ru>
2025-01-16 19:17:52 +0300

    Initial commit

---
+++ src/main/java/applibrarytracker_springdatajpa/applibrarytracker_springdatajpa/AppLibraryTrackerSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package applibrarytracker_springdatajpa.applibrarytracker_springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLibraryTrackerSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLibraryTrackerSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/applibrarytracker_springdatajpa/applibrarytracker_springdatajpa/AppLibraryTrackerSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package applibrarytracker_springdatajpa.applibrarytracker_springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLibraryTrackerSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
